8f0ba41e2b17e3faa09ba21a11f14da0e14b97ff
Zinebbelfaqir <zinebbelfaqir@gmail.com>
2023-02-19 15:03:02 -0800

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
- *
+ * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = 0; j < 100; j++)
 		{
-			printf("%02d %02d, ", i, j);
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
 		}
 	}
-	printf("\n");
-
+	putchar('\n');
 	return (0);
 }
